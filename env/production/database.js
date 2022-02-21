dd259cf83e9eef98fbfb7b84d856ef8a7f9b4350
Thomas Barnwell <thomas@krakenlabs.io>
2022-02-21 14:42:09 -0500

    updating db config

--- env/production/database.js
+++ env/production/database.js
@@ -1,8 +1,21 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-module.exports = ({ env }) => ({
-	connection: {
-		client: 'postgres',
+module.exports = ({ env }) => {
+	const parse = require("pg-connection-string").parse;
+	const config = parse(env("DATABASE_URL", "127.0.0.1"));
+
+	const devConfig = {
+		client: "postgres",
+		connection: {
+			host: env("DATABASE_HOST", "127.0.0.1"),
+			port: env.int("DATABASE_PORT", 5432),
+			database: env("DATABASE_NAME", "db_name"),
+			user: env("DATABASE_USERNAME", "root"),
+			password: env("LOCAL_DB_PASSWORD"),
+			ssl: env.bool("DATABASE_SSL", false),
+		},
+	};
+
+	const prodConfig = {
+		client: "postgres",
 		connection: {
 			host: config.host,
 			port: config.port,
@@ -10,9 +23,11 @@ module.exports = ({ env }) => ({
 			user: config.user,
 			password: config.password,
 			ssl: {
-				rejectUnauthorized: false
+				rejectUnauthorized: false,
 			},
 		},
 		debug: false,
-	},
-});
+	};
+
+	return env("NODE_ENV", "development") === "production" ? prodConfig : devConfig
+};
