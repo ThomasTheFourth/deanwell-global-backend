ebff5bc0d2f02150ac920714c4aa50ba885640bc
Thomas Barnwell <thomas@krakenlabs.io>
2022-03-03 10:36:54 -0500

    Adding release formnat

---
+++ src/api/release-format/controllers/release-format.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  release-format controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::release-format.release-format');
---
+++ src/api/release-format/routes/release-format.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * release-format router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::release-format.release-format');
