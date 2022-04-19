1fa9324cdfa552a0b5495818d4af49c2ca494cd2
Thomas Barnwell <thomas@krakenlabs.io>
2022-04-19 12:28:34 -0400

    Adding product list

--- src/api/products-list/controllers/products-list.js
+++ src/api/products-list/controllers/products-list.js
@@ -1,8 +1,15 @@
 "use strict";
 
 module.exports = {
-  async findAll() {
-    const products = await strapi.query("products").findAll();
-    return products;
+  async findAll(ctx) {
+    const products = strapi.entityService.findMany("api::product.product");
+
+    return (await products).map((product) => {
+      return {
+        id: product.snipCartId,
+        price: product.price,
+        url: "https://d12wr7ozry99zf.cloudfront.net/api/products-list",
+      };
+    });
   },
 };
